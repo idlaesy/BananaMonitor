96da971b041d79f78778d6b5c8b1ec9d9bd03df8
vii <intanoktavia1603@gmail.com>
2024-12-12 23:46:13 +0700

    test commit

---
+++ app/src/main/java/com/banamon/banamon/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.banamon.banamon.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Green = Color(0xFF327039)
+val Yellow = Color(0xFFF0BE49)
+val CreamBase = Color(0xFFF8EDD9)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
